5c31d906a8f12ddb48240309d576990e3e8b0414
joaquinalvidrez95 <joaquin_alan@hotmail.com>
2017-11-15 19:00:00 -0600

    fg

--- MPS/MPS/ColorView.xaml.cs
+++ MPS/MPS/ColorView.xaml.cs
@@ -12,12 +12,35 @@ namespace MPS
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ColorView : ContentView
 	{
-		public ColorView ()
+        string colorName;
+        ColorTypeConverter colorTypeConv = new ColorTypeConverter();
+        public ColorView ()
 		{
 			InitializeComponent ();
 		}
 
-        private void Button_Clicked(object sender, EventArgs e)
+        public string ColorName
+        {
+            set
+            {
+                // Set the name.
+                colorName = value;
+               
+                // Get the actual Color and set the other views.
+                Color color = (Color)colorTypeConv.ConvertFromInvariantString(colorName);
+//                Color color = (Color)colorTypeConv.ConvertFrom(colorName);
+
+                button.BackgroundColor = color;
+               
+            }
+            get
+            {
+                return colorName;
+            }
+        }
+    
+
+    private void Button_Clicked(object sender, EventArgs e)
         {
           
         }
